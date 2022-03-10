649cb0c4708215c36518f1f32b48244e76d5d42d
Jyotika999 <Jyotika9september@gmail.com>
2022-03-10 22:38:25 +0530

    Setup of footer and header done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header'
 import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-        <Header/>
-        <main>
-        <h1>Welcome to Aladin Cart</h1>
-        </main>
-        <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Aladin Cart</h1>
+        </Container>
+      </main>
+      <Footer />
 
     </>
-  
+
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-        Header
+      <Navbar bg="light" expand="lg" collapseOnSelect>
+        <Container fluid>
+          <Navbar.Brand href="#"><i className='fas fa-shopping-cart'></i>Aladin Cart</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              <Nav.Link href="/login"><i className='fas fa-user'></i>SignIn</Nav.Link>
+
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
