28a1b42909297bb2717804ebc07199dde26ba9e5
shee35 <iit2019032@iiita.ac.in>
2022-03-13 18:40:31 +0530

    Added ratings and Products files

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Card } from 'react-bootstrap';
+import Rating from './Ratings'
+
+const Product = ({product}) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+        <a href={`/product/ ${product._id}`}>
+            <Card.Img src={product.image} variant= 'top'></Card.Img>
+        </a>
+        <Card.Body>
+            <a href={`/product/ ${product._id}`}>
+                <Card.Title as='div'>
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </a>
+            <Card.Text as='div' >
+                <Rating 
+                value={product.rating} 
+                Text={`${product.numReviews} Reviews`}
+                color='yellow'></Rating>
+            </Card.Text>
+
+            <Card.Text as='h3'> ${product.price}</Card.Text>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default Product;
