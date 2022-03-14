64aeb8b03cd7b502f52c4eb6491253399edff756
Jyotika999 <Jyotika9september@gmail.com>
2022-03-14 22:56:44 +0530

    Reformatted codes

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,29 +2,35 @@ import React from 'react'
 import { Card } from 'react-bootstrap';
 import Rating from './Ratings'
 
-const Product = ({product}) => {
-  return (
-    <Card className='my-3 p-3 rounded'>
-        <a href={`/product/ ${product._id}`}>
-            <Card.Img src={product.image} variant= 'top'></Card.Img>
-        </a>
-        <Card.Body>
+const Product = ({ product }) => {
+    return (
+        <Card className='my-3 p-3 rounded'>
             <a href={`/product/ ${product._id}`}>
-                <Card.Title as='div'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
+                {/* card image  */}
+                <Card.Img src={product.image} variant='top'></Card.Img>
             </a>
-            <Card.Text as='div' >
-                <Rating 
-                value={product.rating} 
-                Text={`${product.numReviews} Reviews`}
-                color='yellow'></Rating>
-            </Card.Text>
 
-            <Card.Text as='h3'> ${product.price}</Card.Text>
-        </Card.Body>
-    </Card>
-  )
+            {/* card body */}
+            <Card.Body>
+
+                <a href={`/product/ ${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+
+                <Card.Text as='div' >
+                    <Rating
+                        value={product.rating}
+                        Text={`${product.numReviews} Reviews`}
+                        color='yellow'></Rating>
+                </Card.Text>
+
+                <Card.Text as='h3'> ${product.price}</Card.Text>
+            </Card.Body>
+        </Card>
+    )
 }
 
 export default Product;
--- frontend/src/components/Ratings.js
+++ frontend/src/components/Ratings.js
@@ -1,52 +1,55 @@
 import React from 'react'
 // import { propTypes } from 'react-bootstrap/esm/Image';
 
-const Ratings = ({value, Text, color}) => {
-  return (
-    <div className='ratings'>
-        <span>
-            <i style={{color}} className={
-                value>=1
-                ? 'fa-solid fa-star' 
-                : value>=0.5
-                ? 'fa-solid fa-star-half-stroke' 
-                : 'fa-regular fa-star'}></i>
-        </span>
-        <span>
-            <i style={{color}} className={
-                value>=2
-                ? 'fa-solid fa-star' 
-                : value>=1.5
-                ? 'fa-solid fa-star-half-stroke' 
-                : 'fa-regular fa-star'}></i>
-        </span>
-        <span>
-            <i style={{color}} className={
-                value>=3
-                ? 'fa-solid fa-star' 
-                : value>=2.5
-                ? 'fa-solid fa-star-half-stroke' 
-                : 'fa-regular fa-star'}></i>
-        </span>
-        <span>
-            <i style={{color}} className={
-                value>=4
-                ? 'fa-solid fa-star' 
-                : value>=3.5
-                ? 'fa-solid fa-star-half-stroke' 
-                : 'fa-regular fa-star'}></i>
-        </span>
-        <span>
-            <i style={{color}} className={
-                value>=5
-                ? 'fa-solid fa-star' 
-                : value>=4.5
-                ? 'fa-solid fa-star-half-stroke' 
-                : 'fa-regular fa-star'}></i>
-        </span>
-        <span>{Text && Text}</span>
-    </div>
-  )
+
+// creating ratings of all products via stars
+// done via checking the abosilute values >= 1, >=2 , >=3, >=4 , >=5 since we are considering 5 icons for the same
+const Ratings = ({ value, Text, color }) => {
+    return (
+        <div className='ratings'>
+            <span>
+                <i style={{ color }} className={
+                    value >= 1
+                        ? 'fa-solid fa-star'
+                        : value >= 0.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'}></i>
+            </span>
+            <span>
+                <i style={{ color }} className={
+                    value >= 2
+                        ? 'fa-solid fa-star'
+                        : value >= 1.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'}></i>
+            </span>
+            <span>
+                <i style={{ color }} className={
+                    value >= 3
+                        ? 'fa-solid fa-star'
+                        : value >= 2.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'}></i>
+            </span>
+            <span>
+                <i style={{ color }} className={
+                    value >= 4
+                        ? 'fa-solid fa-star'
+                        : value >= 3.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'}></i>
+            </span>
+            <span>
+                <i style={{ color }} className={
+                    value >= 5
+                        ? 'fa-solid fa-star'
+                        : value >= 4.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'}></i>
+            </span>
+            <span>{Text && Text}</span>
+        </div>
+    )
 }
 
 // Ratings.prototype={
