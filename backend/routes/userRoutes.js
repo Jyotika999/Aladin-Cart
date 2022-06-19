d2a9a3f734feccdcc5f12aecfa5aa0c45bb2bdb3
shee35 <iit2019032@iiita.ac.in>
2022-06-19 14:56:36 +0530

    completed the backend part for user register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/user_controller.js'
+import { authUser, registerUser, getUserProfile } from '../controllers/user_controller.js'
 import { protect } from '../middleWare/authMiddleware.js'
 
 router.post('/login', authUser)
+router.route('/').post(registerUser)
 router.route('/profile').get(protect, getUserProfile)
+
 
 export default router;
